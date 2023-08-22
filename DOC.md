4d8b77aa3ef49b7704d0657033656ffa8e4f4bbe
EdVince <2456510228@qq.com>
2023-08-22 15:22:54 +0800

    update

---
+++ DOC.md
@@ -0,0 +1,5 @@
+```
+ - WhisperForConditionalGeneration.generate(transformers/models/whisper/modeling_whisper.py)
+    - GenerationMixin.generate(transformers/generation/utils.py)
+        - GenerationMixin.greedy_search(transformers/generation/utils.py)
+```
---
+++ whisper_models/test_whisper.py
@@ -0,0 +1,24 @@
+from transformers import WhisperProcessor, WhisperForConditionalGeneration
+from datasets import load_dataset
+
+# load model and processor
+processor = WhisperProcessor.from_pretrained("whisper-base.en")
+model = WhisperForConditionalGeneration.from_pretrained("whisper-medium.en")
+
+# load dummy dataset and read audio files
+ds = load_dataset("hf-internal-testing/librispeech_asr_dummy", "clean", split="validation")
+
+sample = ds[0]["audio"]
+input_features = processor(sample["array"], sampling_rate=sample["sampling_rate"], return_tensors="pt").input_features
+print(sample['array'].shape,input_features.shape)
+
+print(input_features)
+
+# generate token ids
+predicted_ids = model.generate(input_features)
+
+# decode token ids to text
+transcription = processor.batch_decode(predicted_ids, skip_special_tokens=False)
+print(transcription)
+transcription = processor.batch_decode(predicted_ids, skip_special_tokens=True)
+print(transcription)
